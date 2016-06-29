87a28918b7b9b2bc130113533ae212414222f5ef
DDGG <ddcatgg@gmail.com>
2016-06-29 15:37:43 +0800

    network/patch.py - patch_ssl()

--- dglib/network/patch.py
+++ dglib/network/patch.py
@@ -1,4 +1,5 @@
 import socket
+import ssl
 
 
 def getaddrinfo_ipv4(host, port, family=0, socktype=0, proto=0, flags=0):
@@ -11,3 +12,17 @@ def patch_getaddrinfo():
 		socket._origin_getaddrinfo = socket.getaddrinfo
 		socket.getaddrinfo = getaddrinfo_ipv4
 
+
+def patch_ssl():
+	"""
+	patch for Python version greater than 2.7.9
+	https://www.python.org/dev/peps/pep-0476/
+	"""
+	try:
+		_create_unverified_https_context = ssl._create_unverified_context
+	except AttributeError:
+		# Legacy Python that doesn't verify HTTPS certificates by default
+		pass
+	else:
+		# Handle target environment that doesn't support HTTPS verification
+		ssl._create_default_https_context = _create_unverified_https_context
