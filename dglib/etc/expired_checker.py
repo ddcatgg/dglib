4e2c7cedff3a9d2ace878c23f48b875dbf22c316
DDGG <ddcatgg@gmail.com>
2016-09-16 14:16:29 +0800

    etc/expired_checker.py - get_http_date()

--- dglib/etc/expired_checker.py
+++ dglib/etc/expired_checker.py
@@ -1,6 +1,7 @@
 import time
 import win32process
 import threading
+from twisted.web.http import stringToDatetime
 
 
 class ExpiredChecker(object):
@@ -23,6 +24,20 @@ class ExpiredChecker(object):
 
 	def on_expired(self):
 		win32process.ExitProcess(0)
+
+
+def get_http_date(url):
+	try:
+		rep = requests.get(url)
+	except:
+		pass
+	else:
+		# get server date
+		server_date = rep.headers.get('date')
+		if server_date:
+			# 1477189234 = time.mktime((2016,10,23,10,20,34,0,0,0))
+			t = stringToDatetime(server_date)
+			return t
 
 
 def test():
