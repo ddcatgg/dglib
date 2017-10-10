97b815b82a6cadb499cd7677e161e40aabe1f6f2
DDGG <ddcatgg@gmail.com>
2017-10-10 17:59:20 +0800

    utils.py - 新增 json_get() 函数

--- dglib/utils.py
+++ dglib/utils.py
@@ -12,19 +12,19 @@ from functools import partial
 
 from basis import ordered_dict
 
-__all__ = ['LockableDict', 'DictObj', 'OrderedDictObj', 'RepeatableTimer', 'QueueEx', 
-	'format_frame', 'str2hex', 'lst2hex', 'mapord', 'mapchr', 'isoformat_date', 
-	'isoformat_time', 'isoformat_datetime', 'std_date', 'std_time', 'std_datetime', 
-	'decode_time', 'dhms', 'unescape_html', 'SafeDumper', 'print_', 'tr', 'getfileext', 
-	'getfilebase', 'extractbaseext', 'changefileext', 'we_are_frozen', 'is_forking', 
-	'module_path', 'module_file', 'extenddir', 'get_argv_param', 'load_table_file', 
-	'dict_it_by_idx', 'val', 'defaultencoding', 'SYS_ENCODING', 'setdefaultencoding', 
-	'nullfile', 'SYS_NULLFILE', 'get_fileversion', 'get_fileversioninfo', 
-	'makesure_dirpathexists', 'redirectSystemStreamsIfNecessary', 'runas_admin', 
-	'disable_deprecationwarnings', 'getconsolehwnd', 'wts_msgbox', 'msgbox', 
-	'set_exit_handler', 'isdebugging', 'make_closure', 'flatten', 'chunk', 'urlencode_uni', 
-	'decode_json', 'html_unescape', 'dget', 'dget_int', 'to_bytes', 'to_bytes_utf8', 'to_unicode', 
-	'to_unicode_utf8']
+__all__ = ['LockableDict', 'DictObj', 'OrderedDictObj', 'RepeatableTimer', 'QueueEx',
+	'format_frame', 'str2hex', 'lst2hex', 'mapord', 'mapchr', 'isoformat_date',
+	'isoformat_time', 'isoformat_datetime', 'std_date', 'std_time', 'std_datetime',
+	'decode_time', 'dhms', 'unescape_html', 'SafeDumper', 'print_', 'tr', 'getfileext',
+	'getfilebase', 'extractbaseext', 'changefileext', 'we_are_frozen', 'is_forking',
+	'module_path', 'module_file', 'extenddir', 'get_argv_param', 'load_table_file',
+	'dict_it_by_idx', 'val', 'defaultencoding', 'SYS_ENCODING', 'setdefaultencoding',
+	'nullfile', 'SYS_NULLFILE', 'get_fileversion', 'get_fileversioninfo',
+	'makesure_dirpathexists', 'redirectSystemStreamsIfNecessary', 'runas_admin',
+	'disable_deprecationwarnings', 'getconsolehwnd', 'wts_msgbox', 'msgbox',
+	'set_exit_handler', 'isdebugging', 'make_closure', 'flatten', 'chunk', 'urlencode_uni',
+	'decode_json', 'json_get', 'html_unescape', 'dget', 'dget_int', 'to_bytes', 
+	'to_bytes_utf8', 'to_unicode', 'to_unicode_utf8']
 
 
 class LockableDict(dict):
@@ -675,6 +675,18 @@ def decode_json(s):
 		except ValueError as e:
 			pass
 	return result
+
+
+def json_get(s, key, default=None):
+	"""
+	解析JSON字符串为字典，然后按.分隔的路径获取键值。
+	"""
+	if isinstance(s, basestring):
+		d = decode_json(s)
+	else:
+		d = s
+	val = dget(d, key, default=default, separator='.')
+	return val
 
 
 def html_unescape(s):
