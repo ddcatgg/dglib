d51b5d017bcdae7eeae8a4cc76b690ea9f238d07
DDGG <ddcatgg@gmail.com>
2018-02-14 02:28:39 +0800

    runonce.py - 使用 runonce.UM_SHOW 函数级属性暂时解决 qt_utils2.UM_SHOW 重新赋值时无法被多个模块共享的问题。

--- dglib/runonce.py
+++ dglib/runonce.py
@@ -6,7 +6,7 @@ import win32con
 import winerror
 import win32event
 
-g_hMutex = None	# 这个mutex不可是局部变量，否则在函数返回时会被释放，起不到互斥作用。
+g_hMutex = None		# 这个mutex不可是局部变量，否则在函数返回时会被释放，起不到互斥作用。
 
 
 def runonce(mutex_name, register_msg="", exit=False):
@@ -19,8 +19,9 @@ def runonce(mutex_name, register_msg="", exit=False):
 	if register_msg:
 		# 如果直接import qt_utils2 会引入对PyQt的依赖，
 		# 造成其他import runonce的程序在py2exe打包的时候体积增大
-		qt_utils2 = __import__("dglib.qt.qt_utils2")
+		qt_utils2 = __import__('dglib.qt.qt_utils2')
 		qt_utils2.UM_SHOW = win32api.RegisterWindowMessage(register_msg)
+		runonce.UM_SHOW = qt_utils2.UM_SHOW
 
 	g_hMutex = win32event.CreateMutex(None, 0, mutex_name)
 	if g_hMutex:
@@ -33,12 +34,12 @@ def runonce(mutex_name, register_msg="", exit=False):
 		else:
 			result = True
 	else:
-		win32api.MessageBox(0, "创建Mutex失败！", "提示", win32con.MB_ICONERROR)
+		win32api.MessageBox(0, '创建Mutex失败！', '提示', win32con.MB_ICONERROR)
 	return result
 
 
-if __name__ == "__main__":
-	runonce("mutex_test", "UM_SHOW_TEST")
+if __name__ == '__main__':
+	runonce('mutex_test', 'UM_SHOW_TEST')
 	while 1:
 		import time
 		time.sleep(1)
