94ed778da26f85d31766ef64a3ea6fe07c3a5393
DDGG <ddcatgg@gmail.com>
2021-04-11 20:34:30 +0800

    upload_to_pypi.py

--- upload_to_pypi.py
+++ upload_to_pypi.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import sys
 import os
 import re
@@ -12,24 +13,32 @@ def make_egg():
 
 
 def make_wheel():
+	try:
+		import wheel
+	except ImportError:
+		raise Exception('You need wheel, pip install wheel!')
 	cmdline = 'python setup.py bdist_wheel'
 	p = subprocess.Popen(cmdline, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	stdout_str = p.stdout.read()
 	stderr_str = p.stderr.read()
 	ret = p.wait()
-	print stdout_str
-	print stderr_str
+	print(stdout_str.decode('gbk'))
+	print(stderr_str.decode('gbk'))
 
 	# creating build\bdist.win32\wheel\dglib-0.1.8.dist-info\WHEEL
-	pattern = r'^creating build\\bdist\.win32\\wheel\\([^-]+)-(.+?)\.dist-info\\WHEEL'
+	pattern = r'^creating build\\bdist\.(?:win32|win-amd64)\\wheel\\([^-]+)-(.+?)\.dist-info\\WHEEL'
 	m = re.search(pattern, stdout_str, re.I|re.M)
 	pkg_name, pkg_ver = None, None
 	if m:
-		pkg_name, pkg_ver = m.groups()
+		pkg_name, pkg_ver = map(bytes.decode, m.groups())
 	return ret, pkg_name, pkg_ver
 
 
 def upload_wheel(pkg_name, pkg_ver, py_ver='2'):
+	try:
+		import twine
+	except ImportError:
+		raise Exception('You need twine, pip install twine!')
 	cmdline = 'twine upload dist/%s-%s-py%s-none-any.whl' % (pkg_name, pkg_ver, py_ver)
 	p = subprocess.Popen(cmdline)
 	ret = p.wait()
@@ -47,7 +56,7 @@ def main():
 		os.system('pause')
 		sys.exit(ret)
 
-	print pkg_name, pkg_ver
+	print(pkg_name, pkg_ver)
 	ret = upload_wheel(pkg_name, pkg_ver)
 	if ret != 0:
 		os.system('pause')
